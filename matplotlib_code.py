d70829172809c8228bda34f6f7ed77fafa39c16c
Meersalzeis <kaifrichter@gmail.com>
2022-10-03 09:34:46 +0200

    changed graphs to bars

--- matplotlib_code.py
+++ matplotlib_code.py
@@ -13,7 +13,11 @@ def display(input_data, prediction_data):
         print("new address")
 
         # First show the input
-        x, y = predictions["real_values"], predictions["time"]
+        x_in_array, y = predictions["real_values"], predictions["time"]
+        x = np.zeros(len(x_in_array))
+        for i in range(len(x_in_array)):
+            cur_x_item = x_in_array[i]
+            x[i] = cur_x_item[0]
 
         plt.subplots(nrows=1, num="pings to "+address)
         plt.title("pings to "+address)
@@ -27,21 +31,22 @@ def display(input_data, prediction_data):
         MLA_name_list = predictions["names"]
         MLA_name = "not set yet"
         
-        for MLA_nr in range(0, len(predictions)-3 ): # -3 for "time", "names" and "real_value" keys, rest are MLAs
+        nr_of_shown_datapoints = len(predictions)-3
+        for MLA_nr in range(0, nr_of_shown_datapoints ): # -3 for "time", "names" and "real_value" keys, rest are MLAs
             current_predictions = predictions[MLA_nr]
             MLA_name = MLA_name_list[MLA_nr]
 
             print("current algorithm = " + MLA_name )
 
             # arrays needed:    should_be, predicted, error, calc_time
-            predicted = current_predictions[0]
+            predicted = current_predictions[0].ravel()
             calc_time = current_predictions[1]
 
             # plot lines
             plt.title(MLA_name)
-            plt.plot(should_be, label = "real rtts")
-            plt.plot(predicted, label = "predicted rtts")
-            plt.plot(calc_time, label = "calculation time")
+            plt.bar(np.arange(len(should_be))-0.15, should_be, width=0.3,  label = "real rtts")
+            plt.bar(np.arange(len(predicted))+0.15, predicted, width=0.3, label = "predicted rtts")
+            #plt.bar(indexes, calc_time, label = "calculation time")
             plt.xlabel("index of data point")
             plt.ylabel("round trip time in ms")
             plt.legend()
